f0f7049f10a0f251a2bbf7ce709aae3eab5aa173
Vaibhav Abhiramacheri <abhiramacherivaibhav@gmail.com>
2024-03-05 22:19:18 +0530

    Pipeline

---
+++ main/main.cpp
@@ -0,0 +1,11 @@
+//File: main.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "Using Pipeline\n";
+  return 0;
+}
